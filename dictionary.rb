737358a9b7caea434c1f2704461be2e1fa11593d
Sergey B <sbrytiuk@lgscout.com>
2017-05-08 15:10:20 +0300

    use hash as node value

--- dictionary.rb
+++ dictionary.rb
@@ -1,6 +1,17 @@
 class Dictionary
   attr_accessor :root_node
 
+  NUMBERS_MAPPING = {
+      2 => %w(a b c),
+      3 => %w(d e f),
+      4 => %w(g h i),
+      5 => %w(j k l),
+      6 => %w(m n o),
+      7 => %w(p q r s),
+      8 => %w(t u v),
+      9 => %w(w x y z),
+  }
+
   def initialize(root = nil)
     @root_node = Node.new(root)
   end
@@ -16,9 +27,15 @@ class Dictionary
 
   def insert_word(word)
     node = root_node
-    word.each_char do |ch|
-      node = insert(node, ch)
+    word.downcase.each_char do |char|
+      number = select_number(char)
+      value = {char => number}
+      node = insert(node, value)
     end
+  end
+
+  def select_number(char)
+    NUMBERS_MAPPING.select { |_, v| v.include?(char) }.keys.first
   end
 
   def insert(node, value)
@@ -35,7 +52,21 @@ class Dictionary
     end
   end
 
-  def search()
+  def build_word(number)
+    number.each do |n|
+
+      search(node, n)
+    end
+  end
+
+  def search(node, n)
+    n_children = node.children.select { |c| c.value.key(n) }
+    value = n_children.value.key(n)
+
+    n_children.each do |n_node|
+      n_node.children.select { |c| c.value.key(n) }
+    end
+
 
   end
 end
@@ -53,3 +84,18 @@ class Node
   end
 end
 
+class Number
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def next
+
+  end
+end
+
+
+require './dictionary.rb'
+d = Dictionary.new
